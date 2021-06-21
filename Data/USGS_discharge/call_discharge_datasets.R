a5db6bae8c676bb6a0fc9de5fbc938f41652690e
LinneaRock <lrock@wisc.edu>
2021-06-21 15:01:27 -0500

    adding data

---
+++ Data/USGS_discharge/call_discharge_datasets.R
@@ -0,0 +1,13 @@
+#call discharge datasets in
+
+
+#Yahara River inlet to Lake Mendota
+YRI_discharge <- read_csv("Data/USGS_discharge/YR-I_discharge.csv")
+#Sixmile Creek
+SMC_discharge <- read_csv("Data/USGS_discharge/SMC_discharge.csv")
+#Dorn Creek
+DC_discharge <- read_csv("Data/USGS_discharge/DC_discharge.csv")
+#Pheasant Branch Creek
+PB_discharge <- read_csv("Data/USGS_discharge/PB_discharge.csv")
+#Yahara River outlet of Lake Mendota
+YRO_discharge <- read_csv("Data/USGS_discharge/YR-O_discharge.csv")  
