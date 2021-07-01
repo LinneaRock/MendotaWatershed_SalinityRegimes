1c10ab8c093b60c25e6e23704e6c228e8a6d299b
LinneaRock <lrock@wisc.edu>
2021-07-01 11:33:52 -0500

    calculating cQ slopes

--- Code/stormflow_baseflow_figures_stats.R
+++ Code/stormflow_baseflow_figures_stats.R
@@ -88,7 +88,10 @@ DC_fullrecord <- fullRecord(DC_events_bf, "DC")
 PB_fullrecord <- fullRecord(PB_events_bf, "PB")
 YRO_fullrecord <- fullRecord(YRO_events_bf, "YR-O")
 
+#bind all data together
 all_full <- bind_rows(YRI_fullrecord, SMC_fullrecord, DC_fullrecord, PB_fullrecord, YRO_fullrecord)
+#save dataset for easy loading later
+saveRDS(all_full, "Data/cQ_slopes/fullRecord.rds")
 
 
 ##Baseflow cQ slopes####
@@ -100,7 +103,11 @@ DC_baseflow <- baseflow(DC_events_bf, "DC")
 PB_baseflow <- baseflow(PB_events_bf, "PB")
 YRO_baseflow <- baseflow(YRO_events_bf, "YR-O")
 
+#bind all data together
 all_baseflow <- bind_rows(YRI_baseflow, SMC_baseflow, DC_baseflow, PB_baseflow, YRO_baseflow)
+#save dataset for easy loading later
+saveRDS(all_baseflow, "Data/cQ_slopes/baseflow.rds")
+
 
 ##individual stormflow cQ slopes####
 source("Code/Functions/eachEvent_cQ_dataframe_function.R")
@@ -111,6 +118,11 @@ source("Code/Functions/eachEvent_cQ_dataframe_function.R")
  PB_events <- each_event_cq(PB_events_bf, "PB")
  YRO_events <- each_event_cq(YRO_events_bf, "YR-O")
 
+ #bind all data together
+ all_individual_events <- bind_rows(YRI_events, SMC_events, DC_events, PB_events, YRO_events)
+ #save dataset for easy loading later
+ saveRDS(all_individual_events, "Data/cQ_slopes/individualEvents.rds")
+ 
 
  ##bulk stormflow cQ slopes####
  source("Code/Functions/bulkEvent_cQ_dataframe_function.R")
@@ -123,3 +135,9 @@ source("Code/Functions/eachEvent_cQ_dataframe_function.R")
  ##The outlet only has a few events that occur in Apr-Jun and Oct-Dec. Need to comment out the other 
  ##two seasons before running. Notes in the function for what to comment out and include. 
  YRO_bulkstorm <- bulk_event_cq(YRO_events_bf, "YR-O")
+ 
+ #bind all data together 
+ all_bulkstorm <- bind_rows(YRI_bulkstorm, SMC_bulkstorm, DC_bulkstorm, PB_bulkstorm, YRO_bulkstorm)
+ #save dataset for easy loading later
+ saveRDS(all_bulkstorm, "Data/cQ_slopes/bulkStormflow.rds")
+ 
