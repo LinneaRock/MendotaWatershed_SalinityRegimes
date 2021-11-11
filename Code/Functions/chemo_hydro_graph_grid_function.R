9ee8920a0de77765e61d6b2082dfb0ac89e4706b
LinneaRock <lrock@wisc.edu>
2021-11-11 16:34:56 -0700

    updating colors in figures

--- Code/Functions/chemo_hydro_graph_grid_function.R
+++ Code/Functions/chemo_hydro_graph_grid_function.R
@@ -82,8 +82,8 @@ chemo_hydro_graph <- function(df.orig, cl_ts_data, rivername){
   
   ggplot() +
     geom_line(df, mapping = aes(date, Daily_dis_cms)) +
-    geom_point(df%>%filter(peak.flag == 1), mapping = aes(date, Daily_dis_cms), color = "#CD5000") +
-    geom_line(df, mapping = aes(date, threshold_peak),color = "#016CA3") +
+    geom_point(df%>%filter(peak.flag == 1), mapping = aes(date, Daily_dis_cms), color = "#E69F00") +
+    geom_line(df, mapping = aes(date, threshold_peak),color = "#0072B2") +
     geom_point(df, mapping = aes(date, keep), color = "purple")
   
   ##-Flag the changes in derivatitives, events is the row of single site which have events
@@ -188,7 +188,7 @@ chemo_hydro_graph <- function(df.orig, cl_ts_data, rivername){
   #chemograph (chloride concentration)
   a <- ggplot(df_all_flow) +
     geom_line(aes(date, all_conc)) +
-    geom_line(aes(date, event_conc), color = "#CD5000") +
+    geom_line(aes(date, event_conc), color = "#E69F00") +
     theme_minimal() +
     labs(x = "", y = "Chloride Concentration"~(mg~L^-1),
          title = rivername) + 
@@ -198,13 +198,13 @@ chemo_hydro_graph <- function(df.orig, cl_ts_data, rivername){
   #hydrograph (discharge)
   b <- ggplot(df_all_flow) +
     geom_line(aes(date, all_dis)) +
-    geom_line(aes(date, event_flow), color = "#CD5000") +
-    geom_ribbon(mapping = aes(x = date, ymin = 0, ymax = eckhardt, fill = "#016CA3")) +
+    geom_line(aes(date, event_flow), color = "#E69F00") +
+    geom_ribbon(mapping = aes(x = date, ymin = 0, ymax = eckhardt, fill = "#0072B2")) +
     theme_minimal() +
     scale_color_manual(labels = "Stormflow",
-                       values = "#CD5000") +
+                       values = "#E69F00") +
     scale_fill_manual(labels = "Eckhardt Baseflow",
-                     values = "#016CA3") +
+                     values = "#0072B2") +
     labs(x = "", y = "Discharge"~(m^3~s^-1)) + 
     theme(legend.title = element_blank(),
           legend.position = "bottom") +
