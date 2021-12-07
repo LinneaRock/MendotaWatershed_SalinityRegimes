fc3881deea8355832a76e4a2e4dc271a7f395337
hdugan <hilarydugan@gmail.com>
2021-12-07 14:43:49 -0600

    changed x-axis labels to show date, and ice-off on lakes

--- Code/zscore_figure.R
+++ Code/zscore_figure.R
@@ -2,7 +2,7 @@
 
 #load datasets with chloride timeseries
 source("Code/estimated_chloride_conc_mass.R")
-#load funciton to flow-normalize and calculate z-score
+#load function to flow-normalize and calculate z-score
 source("Code/Functions/flow_normalize_zscore_function.R")
 
 
@@ -16,15 +16,20 @@ All_flow_norm <- flow_normalize(YRI_ts_mass, "YR-I") %>%
 
 #create the z-score plot
 library(colorblindr)
+library(scales)
 
 ggplot() +
+  geom_vline(aes(xintercept = as.Date('2020-03-22')), linetype = 2) +
+  geom_vline(aes(xintercept = as.Date('2021-03-20')), linetype = 2) +
   geom_line(All_flow_norm %>% filter(ID != "YR-O"), mapping = aes(date, zscore_chloride, group = ID, color = ID)) +
   geom_line(All_flow_norm %>% filter(ID == "YR-O"), mapping = aes(date, zscore_chloride, group = ID, color = ID), size = 1.25) +
   scale_color_OkabeIto() +
   #scale_color_manual(values = wes_palette("Darjeeling1", n = 5, type = "discrete")) +
   theme_minimal() + theme(legend.title = element_blank()) +
-  labs(y = "Z-score of flow-normalized chloride", x = "") 
+  scale_x_date(date_breaks = 'month', labels = date_format("%b")) +
+  labs(y = "Z-score of flow-normalized chloride", x = "") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 ggsave("Figures/F2_flow_norm_cl_zscore.png", width = 6.25, height = 4.25, units = "in", dpi = 500)
-
+  
 
