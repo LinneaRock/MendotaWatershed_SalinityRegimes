a847ab4c03f45e12ea8095ce88d253cefd8743fa
hdugan <hilarydugan@gmail.com>
2022-01-01 12:26:15 -0600

    added deprecated code folder

--- Code/Functions/join_field_cond_function.R
+++ Code/Functions/join_field_cond_function.R
@@ -2,6 +2,6 @@
 join_for_linreg <- function(field_data, logger_data) {
   data <- field_data %>%
     mutate(date = as.POSIXct(round_date(dateTime, unit = "30 minutes"))) %>%
-    left_join(logger_data, by = c("date" = "dateTime")) %>%
-    mutate(SpCond_uScm.x = ifelse(is.na(SpCond_uScm.x), MovingAverage_SpCond_uScm, SpCond_uScm.x))
+    left_join(logger_data, by = c("date" = "dateTime")) #%>%
+    # mutate(SpCond_uScm.x = ifelse(is.na(SpCond_uScm.x), MovingAverage_SpCond_uScm, SpCond_uScm.x))
 }
