75a4e01c187ad46acb1af0d8d1c0c95c91449eb6
hdugan <hilarydugan@gmail.com>
2022-05-25 14:10:50 -0500

    moved figure code to new file

--- Code/cQ_Slope_Figure_Table.R
+++ Code/cQ_Slope_Figure_Table.R
@@ -7,7 +7,6 @@ library(colorblindr)
 library(egg)
 
 ##plot showing seasonal and annual cQ slopes for full record, baseflow, and bulk stormflow####
-
 cQ_slopes_all <- all_full %>% 
   mutate(flow = "Full Record") %>%
   rbind(all_baseflow %>%
@@ -73,88 +72,8 @@ ggsave(
   dpi = 500
 )
 
-
-
-
- ##plot showing individual stormflow events####
-#using all_individual events dataframe, plus 
-all_individual_events$season = factor(all_individual_events$season,
-                                      levels = c("Oct-Dec", "Jan-Mar", "Apr-Jun", "Jul-Sep"))
-
-ggplot() +
-  labs(x = "Stormflow cQ Slope", y = "") +
-  geom_jitter(
-    all_individual_events,
-    mapping = aes(slope_SpC, trib, fill = season),
-    width = 0, height = 0.2,
-    size = 2.5,
-    shape = 21, alpha = 0.8,
-  ) +
-  scale_fill_manual(
-    labels = c("Oct-Dec", "Jan-Mar", "Apr-Jun", "Jul-Sep"),
-    values = palette_OkabeIto[1:4]
-    #values = c("#1DACE8", "#1C366B", "#F24D29", "#E5C4A1")
-  ) +
-  scale_y_discrete(limits=rev) + # flip y axis order for continuity with other plots
-  expand_limits(y = 7) +
-  theme_minimal() +
-  theme(legend.title = element_blank()) +
-  annotate(
-    "rect",
-    xmin = -0.05,
-    xmax = 0.05,
-    ymin = 0,
-    ymax = Inf,
-    alpha = 0.2,
-    color = "grey"
-  ) +
-  annotate(
-    "text",
-    label = 'chemostatic',
-    x = 0.02,
-    y = 5.9,
-    size = 2.5,
-    angle = 90,
-    color = "grey50",
-  ) +
-  geom_point(
-    all_full %>% filter(season == "Annual"),
-    mapping = aes(slope_SpC, trib),
-    shape = "|",
-    size = 6
-  ) +
-  geom_point(
-    all_baseflow %>% filter(season == "Annual"),
-    mapping = aes(slope_SpC, trib),
-    shape = "|",
-    size = 6,
-    color = "#CD5000"
-  ) +
-  geom_point(
-    all_bulkstorm %>% filter(season == "Annual"),
-    mapping = aes(slope_SpC, trib),
-    shape = "|",
-    size = 6,
-    color = "#801129"
-  ) +
-  geom_curve(aes(x = -0.38, y = 6.6, xend = -0.33, yend = 6.4), curvature = -0.5, arrow = arrow(length = unit(0.03, "npc")), col = "#801129") +
-  geom_curve(aes(x = -0.2, y = 6.6, xend = -0.19, yend = 6.4), curvature = -0.1, arrow = arrow(length = unit(0.03, "npc"))) +
-  geom_curve(aes(x = 0.1, y = 6.6, xend = -0.07, yend = 6.4), curvature = 0.5, arrow = arrow(length = unit(0.03, "npc")), color = "#CD5000") +
-  annotate('text', label = 'stormflow', x = -0.51, y = 6.6, hjust = 0, size = 2.5, col = "#801129") +
-  annotate('text', label = 'all', x = -0.24, y = 6.6, hjust = 0, size = 2.5) +
-  annotate('text', label = 'baseflow', x = 0.11, y = 6.6, hjust = 0, size = 2.5, col = "#CD5000")
-
-ggsave(
-  "Figures/F7_individualSlopes.png",
-  height = 3.25,
-  width = 6.25,
-  units = "in",
-  dpi = 500
-)
-
-source("Code/Functions/count_stormflow_events_functions.R")
-
 ##cQ table####
+source("Code/Functions/count_stormflow_events_functions.R")
 
 BFI_tbl <- data.frame(
   Trib = c(
