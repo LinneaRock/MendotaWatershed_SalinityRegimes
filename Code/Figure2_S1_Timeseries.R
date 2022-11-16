613c22a82968e90039f99c88afcb02df02a56f6d
hdugan <hilarydugan@gmail.com>
2022-11-16 08:39:32 -0600

    removed boxplot from Figure 2, made new figure for boxplots

--- Code/Figure2_S1_Timeseries.R
+++ Code/Figure2_S1_Timeseries.R
@@ -1,5 +1,6 @@
 #script to create timeseries figure
 library(scales)
+library(tidyverse)
 #load datasets with chloride timeseries
 source("Code/DataLoad/Estimated_Chloride_Conc_Mass.R") 
 
@@ -35,16 +36,16 @@ all_river_cond |> rbind(SH_cond) |> group_by(ID) |>
   summarise(meanSpC = mean(SpCond_uScm, na.rm = T))
 
 
-p0 = ggplot(all_river_cond |> rbind(SH_cond)) +
-  geom_boxplot(aes(x = ID, y = SpCond_uScm, fill = ID), 
-               size = 0.1, outlier.size = 0.1) +
-  scale_fill_OkabeIto(order = c(1:2,4:8,3)) +
-  theme_bw(base_size = 8) +
-  labs(y = "SpC") +
-  scale_y_log10(limits = c(100,10000), expand = c(0,0.02)) +
-  theme(axis.title.x = element_blank(), 
-        legend.position = 'none', 
-        plot.margin = unit(c(0,0,0,0), "cm"))
+# p0 = ggplot(all_river_cond |> rbind(SH_cond)) +
+#   geom_boxplot(aes(x = ID, y = SpCond_uScm, fill = ID), 
+#                size = 0.1, outlier.size = 0.1) +
+#   scale_fill_OkabeIto(order = c(1:2,4:8,3)) +
+#   theme_bw(base_size = 8) +
+#   labs(y = "SpC") +
+#   scale_y_log10(limits = c(100,10000), expand = c(0,0.02)) +
+#   theme(axis.title.x = element_blank(), 
+#         legend.position = 'none', 
+#         plot.margin = unit(c(0,0,0,0), "cm"))
 
 # Compute the analysis of variance
 res.aov <- aov(SpCond_uScm ~ ID, data = all_river_cond |> rbind(SH_cond))
@@ -94,150 +95,53 @@ p3 = ggplot(all_river_cl) +
         legend.margin = margin(0, 0, 0, 0)) +
   guides(colour = guide_legend(nrow = 1)); p3
 
-(p1 + inset_element(p0, left = 0.4, bottom = 0.55, right = 1, top = 1, align_to = 'full')) / 
-  p2 / p3 +
-  plot_layout(widths = c(2,1.5,1.5)) +
-  plot_annotation(tag_levels = 'a', tag_suffix = ')') & 
-  theme(plot.tag = element_text(size = 8))
+# (p1 + inset_element(p0, left = 0.4, bottom = 0.55, right = 1, top = 1, align_to = 'full')) / 
+#   p2 / p3 +
+#   plot_layout(widths = c(2,1.5,1.5)) +
+#   plot_annotation(tag_levels = 'a', tag_suffix = ')') & 
+#   theme(plot.tag = element_text(size = 8))
+
+ p1 / p2 / p3 +
+plot_layout(widths = c(2,1.5,1.5)) +
+plot_annotation(tag_levels = 'a', tag_suffix = ')') & 
+theme(plot.tag = element_text(size = 8))
 # Save combo plot
 ggsave('Figures/F2_timeseries.png', width = 6.5, height = 6, units = 'in', dpi = 500)
 
   
 ####################### SPRING HARBOR #############################
-SH_cl_estimated <- SH_ts_mass %>%
-  rename(chloride_mgL = chloride_estimated_mgL) %>%
-  filter(dateTime <= max(DC_cl$dateTime)) %>%
-  select(dateTime, chloride_mgL, ID) %>%
-  group_by(as.Date(dateTime), ID) %>%
-  mutate(chloride_mgL = mean(chloride_mgL)) %>%
-  ungroup() %>%
-  dplyr::select(-dateTime) %>%
-  mutate(dateTime = as.POSIXct(`as.Date(dateTime)`)) %>%
-  dplyr::select(-`as.Date(dateTime)`) %>%
-  distinct()
-
 # SpC
-p1 = ggplot(SH_cond |> filter(dateTime >= as.Date("2019-12-19") &
+p1 = ggplot(SH_cond |> filter(dateTime >= as.Date("2014-12-19") &
                                 dateTime < as.Date("2021-04-02"))) +
   geom_line(mapping = aes(dateTime, MovingAverage_SpCond_uScm, 
                           group = ID, color = ID)) +
   scale_color_OkabeIto(order = 3) +
   theme_minimal(base_size = 8) + theme(legend.title = element_blank()) +
-  scale_x_datetime(date_breaks = 'month', labels = date_format("%b")) +
+  scale_x_datetime(date_breaks = 'year', labels = date_format("%Y")) +
   labs(y = "SpC"~(µS~cm^-1), x = "") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
         legend.position = 'none')
 
-#chloride
-p2 = ggplot(SH_cl_estimated) +
-  geom_point(mapping = aes(dateTime, chloride_mgL, 
-                           group = ID, color = ID)) +
-  geom_path(mapping = aes(dateTime, chloride_mgL, 
-                          group = ID, color = ID), size = 0.2, linetype = 2) +
+#Chloride 
+p2 = ggplot(SH_cl |> filter(dateTime >= as.Date("2014-12-19") &
+                              dateTime < as.Date("2021-04-02"))) +
+  geom_point(mapping = aes(dateTime, chloride_mgL, color = ID)) +
+  geom_path(mapping = aes(dateTime, chloride_mgL, color = ID), size = 0.2, linetype = 2) +
   scale_color_OkabeIto(order = 3) +
-  theme_minimal(base_size = 8) + theme(legend.title = element_blank()) +
-  scale_x_datetime(date_breaks = 'month', labels = date_format("%b")) +
-  labs(y = "Chloride"~(mg~L^-1), x = "") +
+  theme_minimal(base_size = 8) + 
+  scale_x_datetime(date_breaks = 'year', labels = date_format("%Y")) +
+  labs(y = "Chloride"~(mg~L^-1)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1), 
-        legend.position = 'bottom', 
+        axis.title.x = element_blank(),
+        legend.position = 'none', 
         legend.title=element_blank(),
         legend.margin = margin(0, 0, 0, 0)) +
-  guides(colour = guide_legend(nrow = 1))
+  guides(colour = guide_legend(nrow = 1)); p2
 
 p1 / p2  +
   plot_annotation(tag_levels = 'a', tag_suffix = ')') & 
   theme(plot.tag = element_text(size = 8))
+
 # Save combo plot
 ggsave('Figures/Supplemental/FigureS1_timeseries_SH.png', width = 6.5, height = 6, units = 'in', dpi = 500)
 
-##################figures including SH###################################################
-
-#bind river conductivity data together
-all_river_cond <- bind_rows(YRI_cond, PB_cond, DC_cond, SMC_cond, YRO_cond, 
-                            SW_cond, YRS_cond, SH_cond) %>%
-  filter(dateTime >= as.Date("2019-12-19") &
-           dateTime < as.Date("2021-04-02"))
-all_river_cond$ID = factor(all_river_cond$ID,
-                           levels = c("DC", "PB","SH", "SMC", "YR-I", "YR-O", "SW", "YR-S"))
-
-#bind chloride data together
-SH_cl_estimated <- SH_ts_mass %>%
-  rename(chloride_mgL = chloride_estimated_mgL) %>%
-  filter(dateTime <= max(DC_cl$dateTime)) %>%
-  select(dateTime, chloride_mgL, ID) %>%
-  group_by(as.Date(dateTime), ID) %>%
-  mutate(chloride_mgL = mean(chloride_mgL)) %>%
-  dplyr::select(-dateTime) %>%
-  rename(dateTime = `as.Date(dateTime)`) %>%
-  distinct()
-
-all_river_cl <- bind_rows(YRI_cl, PB_cl, DC_cl, SMC_cl, YRO_cl, 
-                          SW_cl, YRS_cl, SH_cl_estimated)
-all_river_cl$ID = factor(all_river_cl$ID,
-                         levels = c("DC", "PB", "SH", "SMC", "YR-I", "YR-O", "SW", "YR-S"))
-
-
-
-library(colorblindr)
-library(scales)
-library(patchwok)
-
-#SpC in urban tribs
-p1 = ggplot(all_river_cond |> filter(ID %in% c('SW', 'PB', 'SH'))) +
-  geom_line(mapping = aes(dateTime, MovingAverage_SpCond_uScm, 
-                          group = ID, color = ID)) +
-  scale_color_OkabeIto(order = c(2,3,7)) +
-  theme_minimal(base_size = 8) + theme(legend.title = element_blank()) +
-  scale_x_datetime(date_breaks = 'month', labels = date_format("%b")) +
-  labs(y = "SpC"~(µS~cm^-1), x = "") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
-        legend.position = 'none')
-
-# SpC in ag tribs
-p2 = ggplot(all_river_cond |> filter(!ID %in% c('SW', 'PB', 'SH'))) +
-  geom_line(mapping = aes(dateTime, MovingAverage_SpCond_uScm, 
-                          group = ID, color = ID)) +
-  scale_color_OkabeIto(order = c(1,4,5,6,8)) +
-  theme_minimal(base_size = 8) + theme(legend.title = element_blank()) +
-  scale_x_datetime(date_breaks = 'month', labels = date_format("%b")) +
-  labs(y = "SpC"~(µS~cm^-1), x = "") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
-        legend.position = 'none')
-
-#chloride plot urban tribs
-p3 = ggplot(all_river_cl |> filter(ID %in% c('SW', 'PB', 'SH'))) +
-  geom_point(mapping = aes(dateTime, chloride_mgL, 
-                           group = ID, color = ID)) +
-  geom_path(mapping = aes(dateTime, chloride_mgL, 
-                          group = ID, color = ID), size = 0.2, linetype = 2) +
-  scale_color_OkabeIto(order = c(2,3,7)) +
-  theme_minimal(base_size = 8) + theme(legend.title = element_blank()) +
-  scale_x_datetime(date_breaks = 'month', labels = date_format("%b")) +
-  labs(y = "Chloride"~(mg~L^-1), x = "") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1), 
-        legend.position = 'bottom', 
-        legend.title=element_blank(),
-        legend.margin = margin(0, 0, 0, 0)) +
-  guides(colour = guide_legend(nrow = 1))
-
-#chloride plot ag tribs
-p4 = ggplot(all_river_cl |> filter(!ID %in% c('SW', 'PB', 'SH'))) +
-   geom_point(mapping = aes(dateTime, chloride_mgL, 
-                            group = ID, color = ID)) +
-  geom_path(mapping = aes(dateTime, chloride_mgL, 
-                          group = ID, color = ID), size = 0.2, linetype = 2) +
-  scale_color_OkabeIto(order = c(1,4,5,6,8)) +
-  theme_minimal(base_size = 8) + theme(legend.title = element_blank()) +
-  scale_x_datetime(date_breaks = 'month', labels = date_format("%b")) +
-  labs(y = "Chloride"~(mg~L^-1), x = "") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1), 
-        legend.position = 'bottom', 
-        legend.title=element_blank(),
-        legend.margin = margin(0, 0, 0, 0)) +
-  guides(colour = guide_legend(nrow = 1))
-
-p1 / p3 / p2 / p4 +
-  plot_annotation(tag_levels = 'a', tag_suffix = ')') & 
-  theme(plot.tag = element_text(size = 8))
-# Save combo plot
-ggsave('Figures/FX_timeseries_with_SH.png', width = 6.5, height = 6, units = 'in', dpi = 500)
---
+++ Code/FigureX_Boxplots.R
@@ -0,0 +1,109 @@
+#script to create timeseries figure
+library(scales)
+library(tidyverse)
+library(ggiraphExtra)
+library(patchwork)
+library(multcompView)
+
+#load datasets with chloride timeseries
+source("Code/DataLoad/Estimated_Chloride_Conc_Mass.R") 
+
+#bind river conductivity data together
+all_river_cond <- bind_rows(YRI_cond, PB_cond, DC_cond, SMC_cond, YRO_cond, 
+                            SW_cond, YRS_cond) 
+all_river_cond$ID = factor(all_river_cond$ID,
+                           levels = c("DC", "PB", "SMC", "YR-I", "YR-O", "SW", "YR-S"))
+
+#bind chloride data together
+all_river_cl <- bind_rows(YRI_cl, PB_cl, DC_cl, SMC_cl, YRO_cl, 
+                          SW_cl, YRS_cl)
+all_river_cl$ID = factor(all_river_cl$ID,
+                         levels = c("DC", "PB", "SMC", "YR-I", "YR-O", "SW", "YR-S"))
+
+
+# Plotting 
+ggplot(all_river_cond |> rbind(SH_cond)) +
+  geom_boxplot(aes(x = ID, y = SpCond_uScm, fill = ID), 
+               size = 0.3, outlier.size = 0.1) +
+  scale_fill_OkabeIto(order = c(1:2,4:8,3)) +
+  theme_bw(base_size = 8) +
+  labs(y = "SpC") +
+  scale_y_log10(limits = c(100,10000), expand = c(0,0.02)) +
+  theme(axis.title.x = element_blank(), 
+        legend.position = 'none')
+
+################### SpC boxplot #########################
+df.cond = all_river_cond |> rbind(SH_cond) |> filter(!is.na(MovingAverage_SpCond_uScm))
+# Tukey without SH # analysis of variance
+df.cond.T = df.cond |> 
+  mutate(ID = str_replace_all(ID, "[[:punct:]]", " "))
+
+anova <- aov(MovingAverage_SpCond_uScm ~ ID, data = df.cond.T)
+Tresult = TukeyHSD(anova, ordered = T)
+# compact letter display
+cld <- multcompLetters4(anova, Tresult)
+
+# table with factors and 3rd quantile
+dt <- group_by(df.cond.T, ID) %>%
+  summarise(w=mean(MovingAverage_SpCond_uScm), sd = sd(MovingAverage_SpCond_uScm)) %>%
+  arrange(desc(w))
+
+# extracting the compact letter display and adding to the Tk table
+cld <- as.data.frame.list(cld$ID) |> 
+  rownames_to_column("ID")
+dt = dt |>  mutate(cld = cld$Letters) |> 
+  mutate(ID = str_replace(ID, " ", "-"))
+
+p1 = ggplot(df.cond) +
+  geom_boxplot(aes(y = MovingAverage_SpCond_uScm, x = ID, fill = ID), 
+               size = 0.3, outlier.size = 0.1) +
+  scale_fill_OkabeIto(order = c(1:2,4:8,3)) +
+  geom_text(data = dt, aes(x = ID, label = cld, y = w + sd), vjust = -6, hjust = -0.5, size = 2.5) +
+  theme_minimal(base_size = 8) +
+  labs(y = "SpC"~(µS~cm^-1), x = "") +
+  scale_y_log10(expand = c(0,0.02)) +
+  theme(axis.title.x = element_blank(), 
+        legend.position = 'none')
+
+################### chloride boxplot #########################
+df.cl = all_river_cl |> rbind(SH_cl) |> filter(!is.na(chloride_mgL))
+# Tukey without SH # analysis of variance
+df.cl.T = df.cl |> filter(ID != 'SH') |> 
+  mutate(ID = str_replace_all(ID, "[[:punct:]]", " "))
+         
+anova <- aov(chloride_mgL ~ ID, data = df.cl.T)
+Tresult = TukeyHSD(anova, ordered = T)
+# compact letter display
+cld <- multcompLetters4(anova, Tresult)
+
+# table with factors and 3rd quantile
+dt <- group_by(df.cl.T, ID) %>%
+  summarise(w=mean(chloride_mgL), sd = sd(chloride_mgL)) %>%
+  arrange(desc(w))
+
+# extracting the compact letter display and adding to the Tk table
+cld <- as.data.frame.list(cld$ID) |> 
+  rownames_to_column("ID")
+dt = dt |>  mutate(cld = cld$Letters) |> 
+  mutate(ID = str_replace(ID, " ", "-"))
+
+
+p2 = ggplot(df.cl) +
+  geom_boxplot(aes(y = chloride_mgL, x = ID, fill = ID), 
+               size = 0.3, outlier.size = 0.1) +
+  scale_fill_OkabeIto(order = c(1:2,4:8,3)) +
+  geom_text(data = dt, aes(x = ID, label = cld, y = w + sd), vjust = -6, hjust = -0.5, size = 2.5) +
+  theme_minimal(base_size = 8) +
+  labs(y = "Chloride"~(mg~L^-1), x = "") +
+  scale_y_log10(expand = c(0,0.02)) +
+  theme(axis.title.x = element_blank(), 
+        legend.position = 'none')
+
+############ Join plots ############
+p1 / p2 +
+  plot_annotation(tag_levels = 'a', tag_suffix = ')') & 
+  theme(plot.tag = element_text(size = 8))
+# Save combo plot
+ggsave('Figures/FX_boxplots.png', width = 6.5, height = 6, units = 'in', dpi = 500)
+
+
