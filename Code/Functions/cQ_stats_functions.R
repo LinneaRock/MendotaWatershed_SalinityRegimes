245aa6e100579b28758a49a9df25f9d2090c335a
hdugan <hilarydugan@gmail.com>
2022-11-18 08:57:23 -0600

    got rid of old code

--- Code/Functions/cQ_stats_functions.R
+++ Code/Functions/cQ_stats_functions.R
@@ -41,38 +41,3 @@ addSeason <- function(df) {
                                 levels = c( "Jan-Mar", "Apr-Jun", "Jul-Sep", "Oct-Dec"))
   return(df)
 }
-
-#functions to count types of stormflow events
-
-#cQ slopes < -0.05
-count_dilution_events <- function(df, name) {
-  
-  df <- df %>%
-    filter(trib == name) %>%
-    filter(slope_SpC < -0.05)
-  
-  nrow(df)
-  
-}
-
-#cQ slopes between or equal to -0.05 & 0.05
-count_chemostatic_events <- function(df, name) {
-  
-  df <- df %>%
-    filter(trib == name) %>%
-    filter(slope_SpC >= -0.05 & slope_SpC <= 0.05)
-  
-  nrow(df)
-  
-}
-
-#cQ slopes > 0.05
-count_mobilization_events <- function(df, name) {
-  
-  df <- df %>%
-    filter(trib == name) %>%
-    filter(slope_SpC > 0.05)
-  
-  nrow(df)
-  
-}
